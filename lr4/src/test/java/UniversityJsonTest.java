d0969abff48c2e1480f676fa90ff21115d0815cb
anandrusenko <andrusenkoangela@gmail.com>
2025-11-25 18:31:07 +0200

    Update UniversityJsonTest.java

--- lr4/src/test/java/UniversityJsonTest.java
+++ lr4/src/test/java/UniversityJsonTest.java
@@ -10,21 +10,21 @@ public class UniversityJsonTest {
 
     @Test
     public void testJsonSaveLoad() throws IOException {
-        // 1. Створюємо типовий університет
+        
         UniversityCreator creator = new UniversityCreator();
         University oldUniversity = creator.createTypicalUniversity();
 
-        // 2. Json менеджер
+        
         JsonManager json = new JsonManager();
 
-        // 3. Записуємо у JSON
+        
         String filePath = "university.json";
         json.saveToJson(oldUniversity, filePath);
 
-        // 4. Зчитуємо назад
+        
         University newUniversity = json.loadFromJson(filePath);
 
-        // 5. Перевіряємо, що об’єкти рівні
+        
         assertEquals(oldUniversity, newUniversity);
     }
 }
